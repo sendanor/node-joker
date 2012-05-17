a3876efbc4f61b23b95f51853b5e9fbd149544f2
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-05-18 01:01:33 +0300

    Improved CLI features

--- joker.js
+++ joker.js
@@ -5,20 +5,26 @@
  */
 
 /*
- * API documentation:
+ * Joker.com API documentation:
  *   https://dmapi.joker.com/docs/DMAPI-ext.txt
  */
+
+// FIXME: Change filedb so that it doesn't save backups or it can be opted out
+
+function is_func(f) {
+    if(f && (typeof f === 'function')) return true;
+    return false;
+}
 
 var commands = module.exports = {},
     basedir = process.env.HOME || __dirname,
     db = require('filedb').config({'keep_backup':false}).open(basedir + '/.joker-cli.json'),
     foreach = require('snippets').foreach,
 	rpad = require('snippets').rpad,
-    dmapi = require('joker-dmapi');
+    dmapi = require('joker-dmapi'),
+    cli = require('./cli.js');
 
-// FIXME: Change filedb so that it doesn't save backups or it can be opted out
-
-commands._opts = {'verbose': 0};
+commands._opts_ = {'verbose': 0};
 
 /* Event when database becomes ready */
 db.on('ready', function() {
@@ -28,82 +34,106 @@ db.on('ready', function() {
 	
 	/* */
 	dmapi.on('error', function(err) {
-	    console.log("Error: " + err);
+	    console.error("Error: " + err);
 	});
 	
 	if(db.auth && db.auth.id && db.auth.date && ((new Date()).getTime() - db.auth.date < 58*60*1000 ) ) {
 		dmapi.config({'auth_id':db.auth.id});
 	} else {
 		delete db.auth;
-		db.commit(function(err) { if(err) console.log('error when saving data: ' + err);});
+		db.commit(function(err) { if(err) console.error('error when saving data: ' + err);});
 	}
 });
 
 /* Help command */
-commands.help = function() {
-	console.log('Commands:');
-	console.log('  config list           -- list configuration');
-	console.log('  config set KEY=VALUE  -- set configuration');
-	console.log('  status                -- get session status');
-	console.log('  login USER PW         -- login to DMAPI');
-	console.log('  logout                -- logout');
-	console.log('  query-domain-list     -- list domains');
+commands.help = function(command) {
+	if(command && commands.hasOwnProperty(command)) {
+		cli.init_commands(commands[command], command);
+		if(is_func(commands[command].help)) {
+			commands[command].help();
+		} else {
+			console.log("I'm sorry. This command does not have detailed help available.");
+		}
+	} else {
+		console.log('usage: [--help] COMMAND [ARGS]');
+		console.log("");
+		console.log('The commands are:');
+		console.log('   config             Change settings');
+		console.log('   status             Get session status');
+		console.log('   login              Login to reseller interface');
+		console.log('   logout             Logout from reseller interface');
+		console.log('   domain             Manage domains');
+		console.log("");
+		console.log("See 'joker help COMMAND' for more information on a specific command.");
+	}
 };
 
 /* List domains */
-commands['status'] = function() {
-	db.whenReady(function() {
-		var now = new Date(),
-		    seconds = (db && db.auth && db.auth.date) ? (now.getTime() - db.auth.date) / 1000 : undefined;
-		if(db.auth && db.auth.id && db.auth.date && (seconds < 58*60 ) ) {
-			console.log("Joker: online (" + Math.floor(58*60-seconds) + " seconds left)");
-		} else {
-			console.log("Joker: offline");
-			if(commands._opts.verbose >= 1) {
-				console.log('DEBUG: seconds = ' + seconds);
-				if(db && db.auth && db.auth.date) console.log('DEBUG: db.auth.date = ' + db.auth.date);
-				if(db && db.auth) console.log('DEBUG: db.auth = ' + db.auth);
+commands['status'] = {
+	'__title__': "Get session status",
+	'__desc__': "Print status of joker.com's reseller session (DMAPI).",
+	'_root_': function() {
+		db.whenReady(function() {
+			var now = new Date(),
+			    seconds = (db && db.auth && db.auth.date) ? (now.getTime() - db.auth.date) / 1000 : undefined;
+			if(db.auth && db.auth.id && db.auth.date && (seconds < 58*60 ) ) {
+				console.log("Joker: online (" + Math.floor(58*60-seconds) + " seconds left)");
+			} else {
+				console.log("Joker: offline");
+				if(commands._opts.verbose >= 1) {
+					console.log('DEBUG: seconds = ' + seconds);
+					if(db && db.auth && db.auth.date) console.log('DEBUG: db.auth.date = ' + db.auth.date);
+					if(db && db.auth) console.log('DEBUG: db.auth = ' + db.auth);
+				}
 			}
-		}
-	});
+		});
+	}
 };
 
 /* login -- */
-commands['login'] = function(username, password) {
-	db.whenReady(function() {
-		dmapi.login({'hostname':db.hostname, 'username':username, 'password':password}, function(err, res) { 
-			if(err) {
-				console.log('Error: ' + err);
-				return;
-			}
-			db.auth = {};
-			db.auth.date = (new Date()).getTime();
-			db.auth.id = res.auth_id;
-			db.commit(function(err) { if(err) console.log('error when saving data: ' + err);});
-			console.log('Logged in!');
-			if(commands._opts.verbose >= 1) {
-				console.log('DEBUG: auth_id = ' + res.auth_id);
-			}
+commands['login'] = {
+	'__title__': "Login to DMAPI",
+	'__desc__': "Login to joker.com's reseller interface (DMAPI).",
+	'_root_': function(username, password) {
+		db.whenReady(function() {
+			dmapi.login({'hostname':db.hostname, 'username':username, 'password':password}, function(err, res) { 
+				if(err) {
+					console.error('Error: ' + err);
+					return;
+				}
+				db.auth = {};
+				db.auth.date = (new Date()).getTime();
+				db.auth.id = res.auth_id;
+				db.commit(function(err) { if(err) console.error('error when saving data: ' + err);});
+				console.log('Logged in!');
+				if(commands._opts.verbose >= 1) {
+					console.log('DEBUG: auth_id = ' + res.auth_id);
+				}
+			});
 		});
-	});
+	}
 };
 
 /* logout -- */
-commands['logout'] = function() {
-	db.whenReady(function() {
-		dmapi.logout(function(err) { 
-			if(err) {
-				console.log('Error: ' + err);
-				return;
-			}
-			db.auth = undefined;
-			db.commit(function(err) { if(err) console.log('error when saving data: ' + err);});
-			console.log('Logged out!');
-			if(commands._opts.verbose >= 1) {
-				console.log('DEBUG: auth_id = ' + res.auth_id);
-			}
+commands['logout'] = {
+	'__title__': "Logout from DMAPI",
+	'__desc__': "Logout from joker.com's reseller interface (DMAPI).",
+	'_root_': function() {
+		db.whenReady(function() {
+			dmapi.logout(function(err) { 
+				if(err) {
+					console.error('Error: ' + err);
+					return;
+				}
+				db.auth = undefined;
+				db.commit(function(err) { if(err) console.error('error when saving data: ' + err);});
+				console.log('Logged out!');
+				if(commands._opts.verbose >= 1) {
+					console.log('DEBUG: auth_id = ' + res.auth_id);
+				}
+			});
 		});
-	});
+	}
 };
 
 
@@ -114,16 +144,31 @@ commands['logout'] = function() {
 /* result-delete --  */
 
 /* query-domain-list -- List domains */
-commands['query-domain-list'] = function() {
-	db.whenReady(function() {
-	    dmapi.queryDomainList({}, function(err, domains) {
-			if(err) {
-				console.log('Error: ' + err);
-				return;
-			}
-			console.log(domains);
-	    });
-	});
+commands['domain'] = {
+	'__title__': "Manage domains",
+	'__desc__': "Try 'domain list' to list your domains.",
+	'list': {
+		'__title__': "List domains",
+		'__desc__': 'List of registered domains and their expiration dates (one per line, separated by whitespace). If "showstatus" is present, the the list will be with three columns, the last one showing domain status (like "lock,autorenew" etc - comma separated).',
+		'__opts__': {
+			'pattern': 'Pattern to match (glob-like)',
+			'from': 'Start from this item in list',
+			'to': 'End by this',
+			'showstatus': 'Add additional column, showing domain status, may be 0 or 1.',
+			'showgrants': 'Add additional column, showing domain grants, may be 0 or 1.'
+		},
+		'_root_': function() {
+			db.whenReady(function() {
+			    dmapi.queryDomainList({}, function(err, domains) {
+					if(err) {
+						console.error('Error: ' + err);
+						return;
+					}
+					console.log(domains);
+			    });
+			});
+		}
+	}
 };
 
 /* TODO: query-contact-list --  */
@@ -155,7 +200,27 @@ commands['query-domain-list'] = function() {
 /* TODO: dns-zone-get --  */
 /* TODO: dns-zone-put --  */
 
+/* */
+commands['_root_'] = function() {
+	console.log("Try 'joker help'.");
+};
+
+/* echo */
+commands['echo'] = {
+	'__title__': "Echo arguments",
+	'__desc__': "Test command for CLI code.",
+	'_root_': function() {
+		if(this['__name__']) {
+			console.log('Command: ' + this['__name__']);
+		}
+		console.log('Arguments: ');
+		for(var i = 0; i< arguments.length; ++i) {
+			console.log(arguments[i]);
+		}
+	}
+}
+
 /* Run the framework */
-require('./cli.js').run(commands);
+cli.run(commands);
 
 /* EOF */
